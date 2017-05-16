c5c6e652a6967989a1d0e5a8aa802015dea6fab4
baiyu <baiyu@chinasofti.com>
2017-05-16 17:48:24 +0800

    feat: add join type.

--- cmpt-transform-join/src/main/scala/com/chinasofti/ark/bdadp/component/Join.scala
+++ cmpt-transform-join/src/main/scala/com/chinasofti/ark/bdadp/component/Join.scala
@@ -5,7 +5,7 @@ import java.util
 import com.chinasofti.ark.bdadp.component.api.Configureable
 import com.chinasofti.ark.bdadp.component.api.data.{Builder, SparkData}
 import com.chinasofti.ark.bdadp.component.api.transforms.MultiTransComponent
-import com.chinasofti.ark.bdadp.util.common.StringUtils
+import org.apache.spark.sql.DataFrame
 import org.slf4j.Logger
 
 import scala.collection.JavaConversions._
@@ -17,14 +17,59 @@ class Join(id: String, name: String, log: Logger)
   extends MultiTransComponent[util.Collection[SparkData], SparkData](id, name, log) with
     Configureable {
 
-  var conditionExpr: String = null
+
+  var joinExprs: String = _
+
+  /**
+    * `inner`, `outer`, `left_outer`, `right_outer`, `leftsemi`.
+    */
+  var joinType: String = _
 
   override def apply(inputT: util.Collection[SparkData]): SparkData = {
-    Builder.build(inputT.map(_.getRawData).reduce((f, s) => f.join(s, conditionExpr)))
+
+    /**
+      * @since 1.3.0
+      */
+    val join = (left: DataFrame, right: DataFrame) => left.join(right)
+
+    /**
+      * @since 1.4.0
+      */
+    val joinUsingColumns = (left: DataFrame, right: DataFrame) => {
+      val usingColumns = joinExprs.replace(" ", "").split(",")
+
+      left.join(right, usingColumns)
+    }
+
+    /**
+      * @since 1.6.0
+      */
+    val joinUsingColumnsWithType = (left: DataFrame, right: DataFrame) => {
+      val usingColumns = joinExprs.replace(" ", "").split(",")
+
+      left.join(right, usingColumns, joinType)
+    }
+
+    var rawData: DataFrame = null
+
+    if (joinExprs.isEmpty) {
+      rawData = inputT.map(_.getRawData).reduce(join)
+
+    } else if (joinExprs.nonEmpty && joinType.isEmpty) {
+      rawData = inputT.map(_.getRawData).reduce(joinUsingColumns)
+
+    } else if (joinExprs.nonEmpty && joinType.nonEmpty) {
+      rawData = inputT.map(_.getRawData).reduce(joinUsingColumnsWithType)
+
+    }
+
+    Builder.build(rawData)
   }
 
   override def configure(componentProps: ComponentProps): Unit = {
-    conditionExpr = componentProps.getString("conditionExpr")
-    StringUtils.assertIsBlank(conditionExpr)
+    joinExprs = componentProps.getString("joinExprs", "")
+    joinType = componentProps.getString("joinType", "")
+
   }
+
 }
