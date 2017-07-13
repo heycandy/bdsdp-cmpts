cf30509b7356e65a94acd9ed0539aa1af5609b7e
wangweiwei <672684719@qq.com>
2017-07-13 19:19:59 +0800

    update

--- cmpt-ml-predict-SVM/src/main/scala/com/chinasofti/ark/bdadp/component/SVMPredict.scala
+++ cmpt-ml-predict-SVM/src/main/scala/com/chinasofti/ark/bdadp/component/SVMPredict.scala
@@ -1,48 +1,47 @@
 package com.chinasofti.ark.bdadp.component
 
 import com.chinasofti.ark.bdadp.component.api.Configureable
-import com.chinasofti.ark.bdadp.component.api.data.{Builder, SparkData}
-import com.chinasofti.ark.bdadp.component.api.transforms.TransformableComponent
-import org.apache.commons.lang.StringUtils
+import com.chinasofti.ark.bdadp.component.api.data.{StringData, SparkData}
+import com.chinasofti.ark.bdadp.component.api.sink.{SparkSinkAdapter, SinkComponent}
 import org.apache.spark.mllib.classification.SVMModel
-import org.apache.spark.mllib.linalg.{Vectors, Vector}
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
+import org.apache.spark.mllib.linalg.Vectors
 import org.slf4j.Logger
 
 /**
  * Created by water on 2017.6.29.
  */
-class SVMPredict (id: String, name: String, log: Logger)
-  extends TransformableComponent[SparkData, SparkData](id, name, log) with Configureable {
+class SVMPredict(id: String, name: String, log: Logger)
+  extends SinkComponent[StringData](id, name, log) with Configureable with
+  SparkSinkAdapter[SparkData] with Serializable {
 
   var path: String = null
+  var featuresCol: String = null
 
-  override def apply(inputT: SparkData): SparkData = {
-    val df = inputT.getRawData
-    val sc = df.sqlContext.sparkContext
-    val sameModel = SVMModel.load(sc, path)
-    val data: RDD[Vector] = df.mapPartitions(
+  override def apply(inputT: StringData): Unit = {
+  }
+
+  override def apply(inputT: SparkData): Unit = {
+    var df = inputT.getRawData
+    var featuresColArr: Array[String] = null
+    val sampleModel = SVMModel.load(df.sqlContext.sparkContext, path)
+    if (null != featuresCol && !"".equals(featuresCol)) {
+      featuresColArr = featuresCol.split(",")
+      df = df.selectExpr(featuresColArr: _*)
+    }
+    val featuresData = df.mapPartitions(
       iterator => iterator.map(row => {
         val values = row.toSeq.map(_.toString).map(_.toDouble).toArray
         Vectors.dense(values)
       }))
-    val rddPredict: RDD[Double] = sameModel.predict(data)
-    val rddRow: RDD[Row] = rddPredict.mapPartitions(
-      iterator => iterator.map(row => {
-        Row(row)
-      })
-    )
-    //rdd转换为dataframe
-    val structType = StructType(Array(
-      StructField("label", DoubleType, true)
-    ))
-    val dfResult = df.toDF().sqlContext.createDataFrame(rddRow, structType)
-    Builder.build(dfResult)
+
+    val predictRDD = sampleModel.predict(featuresData)
+    val resultRDD = featuresData.zip(predictRDD)
+    info("====== predict data and result is ======")
+    resultRDD.repartition(8).collect().foreach(row => info(row.toString()))
   }
 
   override def configure(componentProps: ComponentProps): Unit = {
     path = componentProps.getString("path")
+    featuresCol = componentProps.getString("featuresCol")
   }
 }
--- cmpt-ml-predict-decision-tree/src/main/scala/com/chinasofti/ark/bdadp/component/DecisionTreePredict.scala
+++ cmpt-ml-predict-decision-tree/src/main/scala/com/chinasofti/ark/bdadp/component/DecisionTreePredict.scala
@@ -1,46 +1,48 @@
 package com.chinasofti.ark.bdadp.component
 
 import com.chinasofti.ark.bdadp.component.api.Configureable
-import com.chinasofti.ark.bdadp.component.api.data.{Builder, SparkData}
-import com.chinasofti.ark.bdadp.component.api.transforms.TransformableComponent
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import com.chinasofti.ark.bdadp.component.api.data.{StringData, SparkData}
+import com.chinasofti.ark.bdadp.component.api.sink.{SparkSinkAdapter, SinkComponent}
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import org.slf4j.Logger
 
 /**
  * Created by Administrator on 2017/1/12.
  */
 class DecisionTreePredict(id: String, name: String, log: Logger)
-  extends TransformableComponent[SparkData, SparkData](id, name, log) with Configureable {
+  extends SinkComponent[StringData](id, name, log) with Configureable with
+  SparkSinkAdapter[SparkData] with Serializable {
 
   var path: String = null
+  var featuresCol: String = null
 
-  override def apply(inputT: SparkData): SparkData = {
-    val df = inputT.getRawData
-    val sc = df.sqlContext.sparkContext
-    val sameModel = DecisionTreeModel.load(sc, path)
-    val data: RDD[Vector] = df.mapPartitions(
+  override def apply(inputT: StringData): Unit = {
+  }
+
+  override def apply(inputT: SparkData): Unit = {
+    var df = inputT.getRawData
+    var featuresColArr: Array[String] = null
+    val sampleModel = DecisionTreeModel.load(df.sqlContext.sparkContext, path)
+
+    if(null != featuresCol && !"".equals(featuresCol)){
+      featuresColArr = featuresCol.split(",")
+      df = df.selectExpr(featuresColArr: _*)
+    }
+    val featuresData = df.mapPartitions(
       iterator => iterator.map(row => {
         val values = row.toSeq.map(_.toString).map(_.toDouble).toArray
         Vectors.dense(values)
       }))
-    val rddPredict: RDD[Double] = sameModel.predict(data)
-    val rddRow: RDD[Row] = rddPredict.mapPartitions(
-      iterator => iterator.map(row => {
-        Row(row)
-      })
-    )
-    val structType = StructType(Array(
-      StructField("label", DoubleType, true)
-    ))
-    val dfResult = df.toDF().sqlContext.createDataFrame(rddRow, structType)
-    Builder.build(dfResult)
+
+    val predictRDD = sampleModel.predict(featuresData)
+    val resultRDD = featuresData.zip(predictRDD)
+    info("====== predict data and result is ======")
+    resultRDD.repartition(8).collect().foreach(row => info(row.toString()))
   }
 
   override def configure(componentProps: ComponentProps): Unit = {
     path = componentProps.getString("path")
+    featuresCol = componentProps.getString("featuresCol")
   }
 }
