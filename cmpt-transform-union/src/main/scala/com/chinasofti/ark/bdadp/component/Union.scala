be147c2a4a08d0d6c97da40b7e4b604408b22cb8
wangweiwei <672684719@qq.com>
2017-07-27 10:52:50 +0800

    fix : https://10.100.66.119/plugins/tracker/?aid=145

--- cmpt-transform-union/src/main/scala/com/chinasofti/ark/bdadp/component/Union.scala
+++ cmpt-transform-union/src/main/scala/com/chinasofti/ark/bdadp/component/Union.scala
@@ -16,15 +16,15 @@ import scala.collection.JavaConversions._
 class Union(id: String, name: String, log: Logger)
   extends MultiTransComponent[util.Collection[SparkData], SparkData](id, name, log) with Configureable {
 
-  var conditionExpr: String = null
+//  var conditionExpr: String = null
 
   override def apply(inputT: util.Collection[SparkData]): SparkData = {
     Builder.build(inputT.map(_.getRawData).reduce((f, s) => f.unionAll(s)))
   }
 
   override def configure(componentProps: ComponentProps): Unit = {
-    conditionExpr = componentProps.getString("conditionExpr")
-    StringUtils.assertIsBlank(conditionExpr)
+//    conditionExpr = componentProps.getString("conditionExpr")
+//    StringUtils.assertIsBlank(conditionExpr)
   }
 }
 
